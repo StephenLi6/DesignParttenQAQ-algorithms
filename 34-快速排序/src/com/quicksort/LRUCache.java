a3271502161eb280acf3d331dc5d68fdd6b95f92
TheShy <408946395@qq.com>
2021-08-18 09:50:55 +0800

    Create LRUCache.java

---
+++ 34-快速排序/src/com/quicksort/LRUCache.java
@@ -0,0 +1,24 @@
+package com.quicksort
+
+class LRUCache {
+    int capacity;
+    LinkedHashMap<Integer, Integer> cache;
+
+    public LRUCache(int capacity) {
+        this.capacity = capacity;
+        cache = new LinkedHashMap<Integer, Integer>(capacity, 0.75f, true) {
+            @Override
+            protected boolean removeEldestEntry(Map.Entry eldest) {
+                return cache.size() > capacity;
+            }
+        };
+    }
+
+    public int get(int key) {
+        return cache.getOrDefault(key, -1);
+    }
+
+    public void put(int key, int value) {
+        cache.put(key, value);
+    }
+}
