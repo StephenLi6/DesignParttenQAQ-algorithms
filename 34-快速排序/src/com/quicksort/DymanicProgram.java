3f9fe86de376cc2d9079295cb99015a18ba46635
TheShy <408946395@qq.com>
2021-08-19 16:28:46 +0800

    Update DymanicProgram.java

--- 34-快速排序/src/com/quicksort/DymanicProgram.java
+++ 34-快速排序/src/com/quicksort/DymanicProgram.java
@@ -2,6 +2,8 @@ package com.quicksort
 
 /*https://leetcode-cn.com/problems/house-robber/solution/dong-tai-gui-hua-jie-ti-si-bu-zou-xiang-jie-cjavap/
 动态规划问题
+你是一个专业的小偷，计划偷窃沿街的房屋。每间房内都藏有一定的现金，影响你偷窃的唯一制约因素就是相邻的房屋装有相互连通的防盗系统，如果两间相邻的房屋在同一晚上被小偷闯入，系统会自动报警。
+
 */
 public class DymanicProgram {
         public static int rob(int[] nums) {
